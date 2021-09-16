8f242cc98a4f73c196960749eaa6001cb25b9a1f
witalikh <53310378+witalikh@users.noreply.github.com>
2021-09-16 22:18:32 +0300

    Updating task 1 according to recommendations
    * Refactored main() function
    * Improved validation
    * Removed cheating methods from main algorithm

--- Task_1.py
+++ Task_1.py
@@ -1,5 +1,11 @@
+class ImmediateExit(Exception):
+    """Custom class for breaking program"""
+    pass
+
+
 def count_upward_triangles(base: int) -> int:
     """
+    Function that counts upward-oriented triangles
     :param base: count of divisions of main side
     :return: count of upward triangles
     """
@@ -10,39 +16,100 @@ def count_upward_triangles(base: int) -> int:
 
     # although, summation can be found by O(1) formula
     n = base
-    return -n * (n - 1) * (n + 1) // 3 + n * n * (n + 1) // 2
+    return n * n * (n + 1) // 2 - n * (n - 1) * (n + 1) // 3
 
 
 # Idea for O(n): the count of downward triangle per i-th row
 # is an symmetric sum with (i - 1) members
-# i. e. for i = 7, sum = 1 + 2 + 3 + 3 + 2 + 1
+# e. g. for i = 7, sum = 1 + 2 + 3 + 3 + 2 + 1
 # Solution for O(1): code in ready formulas
 def count_downward_triangles(base: int) -> int:
     """
+    Function that counts downward-oriented triangles
     :param base: number of triangles per side
     :return: count of upward triangles
     """
 
-    # odd rows
+    # odd rows of triangles base
     m = (base - 1) // 2 + (base - 1) % 2
 
-    # even rows
+    # even rows of triangles base
     n = (base - 1) // 2
 
-    # sum of upward triangles from odd and even rows
+    # count of upward triangles from odd and even rows
     total_of_evens = n * (n + 1) * (n + 2) // 3
     total_of_odds = (m - 1) * m * (m + 1) // 3 + m * (m + 1) // 2
 
+    # total count of upward triangles
     return total_of_odds + total_of_evens
 
 
+# main algorithm function
 def count_total_triangles(sides: int):
     """
+    Function counting triangles inside big equilateral triangle
     :param sides: number of triangles per side
     :return: total count of triangles
     """
-    # Idea: count upward and downward triangles count and add them
+    # count upward and downward triangles count and add them
     return count_upward_triangles(sides) + count_downward_triangles(sides)
+
+
+# validation of input of required type
+def try_to_input(required_type, input_msg, wrong_input_msg, stop_words):
+    """
+    Function intercepting input and requiring it until correct data is written
+    :param required_type: the type that input data should convert from str
+    :param input_msg: message printed while input
+    :param wrong_input_msg: message warning about incorrect data before asking again
+    :param stop_words: special words (case-independent) to force exit
+    :return: output value in case of success
+    """
+    while True:
+        entry = input(input_msg)
+
+        # if program is needed to be closed beforehand
+        # sorry for fancy method below, it's additional functionality
+        if entry.lower() in stop_words:
+            raise ImmediateExit
+
+        try:
+            # convert to the type
+            output = required_type(entry)
+
+        except ValueError:
+            # type is incompatible
+            print(wrong_input_msg)
+            continue
+
+        else:
+            # type is compatible
+            return output
+
+
+def input_manage(greeting_msg,
+                 input_msg,
+                 wrong_format_msg, wrong_value_msg,
+                 stop_words):
+    """
+    Function intercepting input details
+    :param greeting_msg:
+    :param input_msg:
+    :param wrong_format_msg:
+    :param wrong_value_msg:
+    :param stop_words:
+    :return:
+    """
+    print(greeting_msg, input_msg, sep='\n')
+    output = try_to_input(int, "n = ", wrong_format_msg, stop_words)
+
+    # (notice: triangle with 0 triangles inside is considered as point)
+    if output < 0:
+        print(wrong_value_msg)
+        raise ImmediateExit
+
+    else:
+        return output
 
 
 # main running code
@@ -51,21 +118,43 @@ def main() -> None:
     :return: None
     """
 
-    greeting = """
+    greeting_msg = """
     Задача: знайти кількість рівносторонніх трикутників
     всередині рівностороннього трикутника, якщо вздовж сторони
     можна помістити рівно n рівносторонніх трикутників
-    --------------------------------------------------
     """
-    print(greeting)
 
-    n = input("Введіть число n: ")
-    if n.isnumeric():
-        result = count_total_triangles(int(n))
-        output = "Кількість трикутників при n = " + n + " буде " + str(result)
-        print(output)
+    input_msg = """
+    Введіть число n для обчислення результату
+    Або EXIT в довільному регістрі для виходу"""
+
+    wrong_format_msg = """
+    Введене значення некоректне за форматом. Спробуйте ще раз!"""
+
+    wrong_value_msg = """Для даного числа не існує розв'язку задачі: функція обчислення на ній не визначена"""
+
+    stop_words = ["exit"]
+
+    exit_msg = """Програма передчасно завершила роботу, дякую за увагу!"""
+
+    output_msg = "Максимальна кількість трикутників, " \
+                 "які можна знайти в даному рівносторонньому, дорівнює"
+
+    try:
+        # evaluate an input
+        n = input_manage(greeting_msg,
+                         input_msg,
+                         wrong_format_msg,wrong_value_msg,
+                         stop_words)
+
+    except ImmediateExit:
+        # print a farewell
+        print(exit_msg)
+
     else:
-        print("Некоректно введені дані, розв'язку немає :)")
+        # find a solution and represent it
+        # possibly to encapsulate into function, but insensible
+        print(output_msg, count_total_triangles(n))
 
 
 if __name__ == "__main__":
